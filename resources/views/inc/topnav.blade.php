85709c3491b906c003b42f76553d4346da898acb
unknown <rhnabil.aiub@gmail.com>
2022-10-02 13:46:28 +0600

    5. Form Handle, 6.Form Validation

--- resources/views/inc/topnav.blade.php
+++ resources/views/inc/topnav.blade.php
@@ -1,2 +1,4 @@
 <a class="btn btn-primary" href="{{route('home')}}">Home</a>
 <a class="btn btn-primary" href="{{route('profile')}}">Profile</a>
+<a class="btn btn-primary" href="{{route('studentList')}}">Student List</a>
+<a class="btn btn-primary" href="{{route('studentCreate')}}">Create Student</a>
