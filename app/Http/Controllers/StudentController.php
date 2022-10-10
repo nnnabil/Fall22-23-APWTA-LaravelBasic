ae05d8fbecdb8b63200549d302429f966e8ed1b9
unknown <rhnabil.aiub@gmail.com>
2022-10-10 13:37:43 +0600

    6. Model and CRUD

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,23 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
     public function studentList(){
         $student = array();
-        for($i=0; $i<5; $i++){
-            $student = array(
-                "name" => "Student " . ($i+1),
-                "id" =>"00" . ($i+1)
-            );
-            $students[] = (object)$student; 
-        }
+        // for($i=0; $i<5; $i++){
+        //     $student = array(
+        //         "name" => "Student " . ($i+1),
+        //         "id" =>"00" . ($i+1)
+        //     );
+        //     $students[] = (object)$student; 
+        // }
         // return $students;
+        $students = Student::all(); 
         return view('student.studentList')->with('students', $students);
     }
     public function studentEdit(Request $request){
-        return $request->name;
+        $student = Student::where('id', $request->id)->first();
+        // return $student;
+        return view('student.studentEdit')->with('student', $student);
+        // return view('student.studentCreate')->with('student', $student);
+    }
+    public function studentEditSubmitted(Request $request){
+        $student = Student::where('id', $request->id)->first();
+        // return  $student;
+        $student->name = $request->name;
+        $student->id = $request->id;
+        $student->save();
+        return redirect()->route('studentList');
+
+    }
+
+    public function studentDelete(Request $request){
+        $student = Student::where('id', $request->id)->first();
+        $student->delete();
+
+        return redirect()->route('studentList');
     }
     public function studentCreate(){
         return view('student.studentCreate');
@@ -34,6 +55,12 @@ class StudentController extends Controller
         ],
         ['name.required'=>"Please put you name here"]
     );
-        return $request;
+        $student = new  Student();
+        $student->name = $request->name;
+        $student->id = $request->id;
+        $student->email = $request->email;
+        $student->save();
+
+        return redirect()->route('studentList');
     }
 }
